f4efbd79c75349dfc45d6f43d598aaef86aa01e1
Peter Hallam <pragmatic@cpan.org>
2012-07-17 18:43:13 +0800

    Initial commit with dzil new

---
+++ lib/List/Compare/Lite.pm
@@ -0,0 +1,35 @@
+#  COPYRIGHT: © 2012 Peter Hallam
+
+package List::Compare::Lite;
+
+#   ABSTRACT: This module does something
+#    CREATED: Tue, 17 Jul 2012 10:43:13 UTC
+#     AUTHOR: Peter Hallam <pragmatic@cpan.org>
+
+use strict;
+use warnings;
+use 5.010001;
+
+# VERSION
+
+
+
+1;
+
+__END__
+
+=encoding utf8
+
+=for Pod::Coverage
+
+=head1 SYNOPSIS
+
+ use List::Compare::Lite;
+
+ my $lite = List::Compare::Lite->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+# vim:set ts=4 sw=4 et ft=perl:
